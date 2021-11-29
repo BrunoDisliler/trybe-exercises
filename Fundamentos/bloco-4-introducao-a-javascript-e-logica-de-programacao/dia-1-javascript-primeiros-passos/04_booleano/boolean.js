44ec6019131e77d7d103226cd541fd337525bea4
Bruno Disliler <bruno_disliler@outlook.com>
2021-11-29 08:56:46 -0300

    Adicionando 9 ex

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/04_booleano/boolean.js
@@ -0,0 +1,7 @@
+const a = -100;
+
+if (a >= 0){
+  console.log('positive');
+} else {
+  console.log('negative');
+}
---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/08_constantes_par/const.js
@@ -0,0 +1,9 @@
+const a = prompt('Digite um número: ');
+const b = prompt('Digite um número: ');
+const c = prompt('Digite um número: ');
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
+  alert('true');
+} else{
+  alert('false');
+}
