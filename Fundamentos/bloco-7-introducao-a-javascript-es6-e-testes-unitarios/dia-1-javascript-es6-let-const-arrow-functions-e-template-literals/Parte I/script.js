8d2b85a7bc9fe43fb32d536120382f744b1084cb
Bruno Disliler <bruno_disliler@outlook.com>
2022-01-12 18:15:12 -0300

    Exercicio 1 - Parte I

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Parte I/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/Parte I/script.js
@@ -1,18 +1,18 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-}
+// Exercício Original
 
-testingScope(true);
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
 
-const testingScope = escopo => ``
+// testingScope(true);
 
 /* 
 Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
@@ -20,3 +20,17 @@ Modifique a estrutura da função para que ela seja uma arrow function .
 Modifique as concatenações para template literals .
 Copie o código abaixo. 
 */
+
+const testingScope = escopo => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
