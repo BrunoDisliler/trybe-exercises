dda0d4498803d199f8719751f6630169947ac650
Bruno Disliler <bruno_disliler@outlook.com>
2022-01-17 10:25:32 -0300

    teste retorno funçao zero

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,10 @@
+const { it, expect } = require('@jest/globals');
 const sum = require('./sum');
 
 test('Soma entre dois números', () => {
   expect(sum(4, 5)).toEqual(9);
+})
+
+it('Soma entre 0 = 0 será 0', () => {
+  expect(sum(0, 0)).toEqual(0);
 })
