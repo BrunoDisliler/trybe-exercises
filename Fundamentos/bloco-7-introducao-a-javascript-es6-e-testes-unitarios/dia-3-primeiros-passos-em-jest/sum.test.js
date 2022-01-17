132a8d9233c695918bfa892671c2e53917698fba
Bruno Disliler <bruno_disliler@outlook.com>
2022-01-17 10:35:34 -0300

    Finalizando Ex 01

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,7 +1,10 @@
 const { it, expect } = require('@jest/globals');
+const exp = require('constants');
+const expectExport = require('expect');
+const { describe } = require('yargs');
 const sum = require('./sum');
 
-test('Soma entre dois números', () => {
+it('Soma entre dois números', () => {
   expect(sum(4, 5)).toEqual(9);
 })
 
@@ -10,5 +13,9 @@ it('Soma entre 0 = 0 será 0', () => {
 })
 
 it('Retorna erro quando inserir strings com número', () => {
-  expect(sum(4, '5')).toEqual('Erro');
+  expect(() => sum(4, '5')).toThrowError();
+})
+
+it('Retorna uma string contendo erro específico', () => {
+  expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
 })
