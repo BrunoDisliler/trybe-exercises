8e93aefe2fb1bf67303a5ba597cbac4bbf31dc28
Bruno Disliler <bruno_disliler@outlook.com>
2022-01-17 10:27:18 -0300

    teste erro

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -8,3 +8,7 @@ test('Soma entre dois números', () => {
 it('Soma entre 0 = 0 será 0', () => {
   expect(sum(0, 0)).toEqual(0);
 })
+
+it('Retorna erro quando inserir strings com número', () => {
+  expect(sum(4, '5')).toEqual('Erro');
+})
