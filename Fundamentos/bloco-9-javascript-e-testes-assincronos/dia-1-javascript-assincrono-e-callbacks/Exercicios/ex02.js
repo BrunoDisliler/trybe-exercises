f91be0f06aa354d0cf89be178fdc6670118a9b0f
Bruno Disliler <bruno_disliler@outlook.com>
2022-02-09 21:49:01 -0300

    Bloco 9 - Dia 1 - Exercicio 2

--- Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/Exercicios/ex02.js
+++ Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/Exercicios/ex02.js
@@ -1,14 +1,38 @@
 // 2 - Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
 
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  console.log("Returned planet: ", mars);
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+  value: 227900000,
+  measurementUnit: "kilometers",
+  },
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+  value: 108200000,
+  measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+  value: 778500000,
+  measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+/* 
+O primeiro a ser executado será o A, pois não há nenhuma condição que diga ao contrário.
+O segundo a ser executado será o C, pois o B tem uma função que pede para aguardar 3000 milisegundos, 
+enquanto que a B são 2000 milisegundos.
+*/
