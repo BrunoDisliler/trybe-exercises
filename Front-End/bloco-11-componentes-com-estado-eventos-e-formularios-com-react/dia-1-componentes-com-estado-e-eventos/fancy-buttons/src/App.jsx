97a09c6f2df50c24b871a6c390d91d67aef0cee6
BrunoDisliler <bruno_disliler@outlook.com>
2022-02-22 08:56:50 -0300

    Exercício 5

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
@@ -5,9 +5,11 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
-  // // A função `super()` é chamada para garantir que a lógica interna do React rode **antes** da sua. Se não for assim, o código não funcionará
+    // // A função `super()` é chamada para garantir que a lógica interna do React rode **antes** da sua. Se não for assim, o código não funcionará
     super()
-    // // Sua lógica extra vai aqui! O parâmetro `props` é opcional, para você acessar as props diretamente no construtor
+    this.state = {
+      clicks: 0 
+    }
     this.button = this.button.bind(this);
   }
 
