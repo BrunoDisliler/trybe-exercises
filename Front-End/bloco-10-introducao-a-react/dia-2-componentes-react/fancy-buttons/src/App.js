f547ce6ab0b228e7e4b6de1f39e882233d9cc2c1
BrunoDisliler <bruno_disliler@outlook.com>
2022-03-06 22:58:59 -0300

    Exercicio 08 e 09 - Fixaçao

--- Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
@@ -10,36 +10,55 @@ class App extends React.Component {
     this.button3 = this.button3.bind(this);
 
     this.state = {
-      clicksutton1: 0,
+      clickButton1: 0,
       clickButton2: 0,
-      clicksButton3: 0,
+      clickButton3: 0,
     }
   }
 
   button1() {
-    this.setState((prevState) => ({
-      clicksutton1: prevState.clicksutton1 + 1,
+    this.setState(({clickButton1}) => ({
+      clickButton1: clickButton1 + 1,
     }))
   };
   
   button2() {
-    this.setState((prevState) => ({
-      clickButton2: prevState.clickButton2 + 1,
+    this.setState(({clickButton2}) => ({
+      clickButton2: clickButton2 + 1,
     }))
   };
   
   button3() {
-    this.setState((prevState) => ({
-      clicksButton3: prevState.clicksButton3 + 1,
+    this.setState(({clickButton3}) => ({
+      clickButton3: clickButton3 + 1,
     }))
   };
 
+  ChangeButtonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
+    const { clickButton1, clickButton2, clickButton3 } = this.state;
     return (
       <div className='divButtons'>
-        <button onClick={this.button1}>Botão 1 | Count = {this.state.clicksutton1}</button>
-        <button onClick={this.button2}>Botão 2 | Count = {this.state.clickButton2}</button>
-        <button onClick={this.button3}>Botão 3 | Count = {this.state.clicksButton3}</button>
+        <button 
+          onClick={this.button1}
+          style={{ backgroundColor: this.ChangeButtonColor(clickButton1) }}
+          >Botão 1 | Count = {this.state.clickButton1}
+        </button>
+
+        <button 
+          onClick={this.button2}
+          style={{ backgroundColor: this.ChangeButtonColor(clickButton2) }}
+          >Botão 2 | Count = {this.state.clickButton2}
+        </button>
+
+        <button 
+          onClick={this.button3}
+          style={{ backgroundColor: this.ChangeButtonColor(clickButton3) }}
+          >Botão 3 | Count = {this.state.clickButton3}
+        </button>
       </div>
     );
   }
