40ae14124d0627c6dc4b15e29ce0b17b0955a4f3
BrunoDisliler <bruno_disliler@outlook.com>
2022-03-06 22:33:56 -0300

    Exercicio 05 - Fixaçao

--- Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
@@ -5,20 +5,38 @@ class App extends React.Component {
   constructor() {
     super()
 
-    this.button = this.button.bind(this);
+    this.button1 = this.button1.bind(this);
+    this.button2 = this.button2.bind(this);
+    this.button3 = this.button3.bind(this);
+
+    this.state = {
+      clicksutton1: 0,
+      clickButton2: 0,
+      clicksButton3: 0,
+    }
   }
 
-  button() {
+  button1() {
     console.log(this);
-    return console.log('Clicou no botão');
+    return console.log('Clicou no botão 1');
+  }
+  
+  button2() {
+    console.log(this);
+    return console.log('Clicou no botão 2');
+  }
+  
+  button3() {
+    console.log(this);
+    return console.log('Clicou no botão 3');
   }
 
   render() {
     return (
       <div className='divButtons'>
-        <button onClick={this.button}>Botão 1</button>
-        <button onClick={this.button}>Botão 2</button>
-        <button onClick={this.button}>Botão 3</button>
+        <button onClick={this.button1}>Botão 1</button>
+        <button onClick={this.button2}>Botão 2</button>
+        <button onClick={this.button3}>Botão 3</button>
       </div>
     );
   }
