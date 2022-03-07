07617fb27853df2e08ef9b859cd48f6749b5c435
BrunoDisliler <bruno_disliler@outlook.com>
2022-03-06 22:27:01 -0300

    Exercicio 03 - Fixaçao

--- Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fancy-buttons/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import React from 'react';
 
-function button1() {
-  return console.log('Clicou no botão 1');
-}
-
-function button2() {
-  return console.log('Clicou no botão 2');
-}
-
-function button3() {
-  return console.log('Clicou no botão 3');
-}
 class App extends React.Component {
+  button1() {
+    return console.log('Clicou no botão 1');
+  }
+  
+  button2() {
+    return console.log('Clicou no botão 2');
+  }
+  
+  button3() {
+    return console.log('Clicou no botão 3');
+  }
+
   render() {
     return (
       <div className='divButtons'>
-        <button onClick={button1}>Botão 1</button>
-        <button onClick={button2}>Botão 2</button>
-        <button onClick={button3}>Botão 3</button>
+        <button onClick={this.button1}>Botão 1</button>
+        <button onClick={this.button2}>Botão 2</button>
+        <button onClick={this.button3}>Botão 3</button>
       </div>
     );
   }
