21204fa12e5df35a335542ae8d52c99723d9081e
Bruno Disliler <bruno_disliler@outlook.com>
2022-05-03 11:51:20 -0300

    Ex Fixação 08

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -31,12 +31,36 @@ export default class App extends React.Component {
     }))
   };
 
+  changeColor = (num) => {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
+    const { numeroDeCliques1, numeroDeCliques2,  numeroDeCliques3 } = this.state;
     return (
       <div style={{display: 'flex'}}>
-        <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
-        <button onMouseOver={this.handleClick2}>{this.state.numeroDeCliques2}</button>
-        <button onDoubleClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
+
+        <button 
+          onClick={this.handleClick1}
+          style={{ backgroundColor: this.changeColor(numeroDeCliques1) }}
+          >
+            Botão 1 | Count = { numeroDeCliques1 }
+        </button>
+
+        <button 
+          onMouseOver={this.handleClick2}
+          style={{ backgroundColor: this.changeColor(numeroDeCliques2) }}
+          >
+            Botão 2 | Count = { numeroDeCliques2 }
+        </button>
+
+        <button 
+          onDoubleClick={this.handleClick3}
+          style={{ backgroundColor: this.changeColor(numeroDeCliques3) }}
+          >
+            Botão 3 | Count = { numeroDeCliques3 }
+        </button>
+
       </div>
     );
   }
