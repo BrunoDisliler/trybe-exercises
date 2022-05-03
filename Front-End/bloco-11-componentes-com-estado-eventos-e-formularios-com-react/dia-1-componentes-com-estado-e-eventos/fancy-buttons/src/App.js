a67b09e9c82f6df12c0a2090f408e8e351d435ef
Bruno Disliler <bruno_disliler@outlook.com>
2022-05-03 11:20:00 -0300

    Ex Fixação 05, 06 & 07

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,29 +3,40 @@ import React from 'react';
 export default class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    }
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
   }
 
-  handleClick1() {
-    alert('Clicou 1');
-  }
+  handleClick1 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
+  };
   
-  handleClick2() {
-    alert('Clicou 2');
-  }
+  handleClick2 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
+  };
   
-  handleClick3() {
-    alert('Clicou 3');
-  }
+  handleClick3 = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }))
+  };
 
   render() {
     return (
       <div style={{display: 'flex'}}>
-        <button onClick={this.handleClick1}>Click 1</button>
-        <button onMouseOver={this.handleClick2}>Click 2</button>
-        <button onDoubleClick={this.handleClick3}>Click 3</button>
+        <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
+        <button onMouseOver={this.handleClick2}>{this.state.numeroDeCliques2}</button>
+        <button onDoubleClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
       </div>
     );
   }
