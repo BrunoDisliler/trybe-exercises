eccf9b93ebad0a5490601c6bd10faff6ff193fd9
Bruno Disliler <bruno_disliler@outlook.com>
2022-05-03 10:58:37 -0300

    Ex Fixação 02

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-function handleClick() {
-  alert('Clicou');
+function handleClick1() {
+  alert('Clicou 1');
+}
+
+function handleClick2() {
+  alert('Clicou 2');
+}
+
+function handleClick3() {
+  alert('Clicou 3');
 }
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <button onClick={handleClick}>Click</button>
+      <div style={{display: 'flex'}}>
+        <button onClick={handleClick1}>Click 1</button>
+        <button onMouseOver={handleClick2}>Click 2</button>
+        <button onDoubleClick={handleClick3}>Click 3</button>
       </div>
     );
   }
