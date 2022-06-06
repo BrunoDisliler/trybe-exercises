89ae70d93456bcffee37843975fc43df4c771792
Bruno Disliler <bruno_disliler@outlook.com>
2022-06-06 17:27:46 -0300

    Exercicios Dia

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicios/imc.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicios/imc.js
@@ -1,4 +1,13 @@
-const peso = 90
-const altura = 2
-const imc = (peso / altura) * 2
-console.log(imc);
+const readLine = require('readline-sync')
+
+console.log('Calculadora de IMC')
+
+const w = readLine.question('Digite seu peso: ')
+const h = readLine.question('Digite sua altura: ')
+
+function imc(peso, altura) {
+  console.log((peso / altura) * 2);
+}
+  
+
+console.log(`Seu IMC baseado em sua altura de ${h} e seu peso de ${w}, é de: ${imc()}`);
