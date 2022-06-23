0400533339ab0ade2b64165a66c5f01e09beab0a
Bruno Disliler <bruno_disliler@outlook.com>
2022-06-23 16:28:47 -0300

    Exercício 02

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
@@ -8,9 +8,15 @@ const message = {
   "message": "pong" 
 }
 
-app.get('/ping', function(req, res) {
+app.get('/ping', (req, res) => {
   res.json(message);
 })
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.status(200).json({ message: `Hello, ${name}` });
+});
+
 
 app.listen(3001, () => {
   console.log('Sendo executado na porta 3001');
