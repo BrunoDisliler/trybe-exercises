589441f17774d53ad3cb8125ed1c137355eee225
Bruno Disliler <bruno_disliler@outlook.com>
2022-06-23 16:44:06 -0300

    Exercício 04

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
@@ -19,9 +19,15 @@ app.post('/hello', (req, res) => {
 
 app.post('/greetings', (req, res) => {
   const { name, age } = req.body;
-  if (age > 17) {
-    return res.status(200).json({ message: `Hello ${name}` })
-  } return res.status(401).json({ message: "Unauthorized" })
+  if (Number(age) <= 17) {
+    return res.status(401).json({ message: "Unauthorized" })
+  } return res.status(200).json({ message: `Hello ${name}` })
+})
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  return res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade`})
+
 })
 
 
