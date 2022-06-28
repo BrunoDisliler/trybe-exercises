3f85c37db0650dc7fb405467a767fabdf193c2bb
Bruno Disliler <bruno_disliler@outlook.com>
2022-06-28 14:22:54 -0300

    Exercicio 02

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios-do-dia/index.js
@@ -4,9 +4,14 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-app.get('/ping', (_req, res) => {
-  return res.status(200).json({ message: 'pong' })
+app.get('/ping', (_req, res) => {res.json( { message: 'pong' } )})
+
+app.post('/hello', (req, res) => {
+  const { name="Bruno" } = req.body
+  return res.status(201).json( { message: `Hello, ${name}`} )
 })
+
+
 
 app.listen('3002', () => {
   console.log('Aplicação ouvindo na porta 3002')
