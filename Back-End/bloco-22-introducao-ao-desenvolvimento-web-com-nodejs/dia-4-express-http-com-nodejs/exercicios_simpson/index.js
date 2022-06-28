22142362720ec1ce27f63582a53bd4edc9ad5bdf
Bruno Disliler <bruno_disliler@outlook.com>
2022-06-28 15:25:47 -0300

    Exercicio 07

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios_simpson/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicios_simpson/index.js
@@ -19,4 +19,17 @@ app.get('/simpsons', async (_req, res) => {
   }
 })
 
+app.get('/simpsons/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const simpsons = await getSimpsons()
+    const simpson = simpsons.find(({ id }) => id === req.params.id)
+    if (!simpson) return res.status(404).json({ message: "Simpson not found" })
+
+    return res.status(202).json(simpson)
+  } catch(error) {
+    res.status(500).end()
+  }
+}) 
+
 app.listen('3003', () => { console.log('Ouvindo na porta 3003') })
