e1ee97ad830a31022a35808ec797444b875454d3
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-04 09:03:10 -0300

    Exercio 01 Fixação

---
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Books.js
@@ -0,0 +1,20 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [books] = await connection.execute('SELECT * FROM model_example.books;');
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+};
+
+module.exports = {
+  getAll,
+};
+
+// Crie um método getByAuthorId no modelo Book, 
+// para retornar apenas livros associados com um determinado author_id. 
+// Altere o middleware da rota books criado no passo 2 
+// para receber uma query string com a chave author_id, e retornar apenas os livros associados.
