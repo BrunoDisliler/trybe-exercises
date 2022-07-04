09fac300326feef9bfedaf309afbce595010858e
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-04 09:24:18 -0300

    Exercio 05 Fixação

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Books.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Books.js
@@ -18,11 +18,36 @@ const getAuhorById = async (authorId) => {
     title,
     authorId: author_id,
   }));
-}
+};
+
+const getById = async (id) => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;';
+  const [books] = await connection.execute(query, [id]);
+  if (books.length === 0) return null;
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+};
+
+const isValid = async (title, authorId) => {
+  if (!title || typeof title !== 'string' || title.length < 3) return false;
+  if (!authorId || typeof authorId !== 'number' || !(await Author.findById(authorId))) return false;
+  return true;
+};
+
+const create = async (title, authorId) => connection.execute(
+'INSERT INTO model_example.books (title, author_id) VALUES (?,?)',
+[title, authorId],
+);
 
 module.exports = {
   getAll,
   getAuhorById,
+  getById,
+  isValid,
+  create,
 };
 
 // Crie um método getByAuthorId no modelo Book, 
