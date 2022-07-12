c3b4b848084d39fa195f3ade538a68f9e2cc8313
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-12 14:44:36 -0300

    Exercício Bônus - Fibonacci

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
@@ -1,7 +1,7 @@
 const readLine = require("readline-sync");
 
 const script = readLine.question(
-  "Digite qual script você quer executar:\n1 - Cálculo IMC\n2 - Velocidade Média\n3 - Sorteio\n\n"
+  "Digite qual script você quer executar:\n1 - Cálculo IMC\n2 - Velocidade Média\n3 - Sorteio\n4 - Fatorial\n5 - Fibonacci\n\n"
 );
 if (script === "1") {
   const imc = require("./imc");
@@ -14,4 +14,12 @@ if (script === "2") {
 if (script === "3") {
   const sorteio = require("./sorteio");
   sorteio();
+}
+if (script === "4") {
+  const fatorial = require("./fatorial");
+  fatorial();
+}
+if (script === "5") {
+  const fibonacci = require("./fibonacci");
+  main();
 }
