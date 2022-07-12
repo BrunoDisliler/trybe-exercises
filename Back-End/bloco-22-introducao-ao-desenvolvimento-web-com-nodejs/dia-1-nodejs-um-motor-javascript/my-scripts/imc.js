bf784bec9f310dfe78e9f7b112b1fc829e359e28
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-11 22:57:43 -0300

    Exercicio 05 - Condicionais

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -8,6 +8,12 @@ const calcImc = () => {
   const imc = peso / (alturaQuadrado)
 
   console.log(`IMC: ${imc.toFixed(2)}kg/m2`) 
-}
 
-calcImc();
+  if (imc < 18.5) {console.log('Situação: Abaixo do peso (magreza)')}
+  if (imc > 18.5 && imc < 24.9) {console.log('Situação: Peso normal')}
+  if (imc > 25 && imc < 29.9) {console.log('Situação: Acima do peso (sobrepeso)')}
+  if (imc > 30 && imc < 34.9) {console.log('Situação: Obesidade grau I')}
+  if (imc > 35 && imc < 39.9) {console.log('Situação: Obesidade grau II')}
+  if (imc > 40) {console.log('Situação: Obesidade grau III e IV')}
+}
+calcImc()
