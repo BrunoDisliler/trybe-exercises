337557c020c62bd8b3de29e3ad9eabbbc29057bb
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-19 11:02:37 -0300

    Exercício 02 e 03

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicios-do-dia/calculo.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicios-do-dia/calculo.js
@@ -1,10 +1,16 @@
 const calculo = (num) => {
-  if (num > 0) {
-    return 'positivo';
-  }
-  if (num < 0) {
-    return 'negativo';
-  } return 'neutro';
-}
+    if (num > 0) {
+      return 'positivo';
+    };
+    if (num < 0) {
+      return 'negativo';
+    }
+    if (num === 0) { 
+      return 'neutro';
+    }
+    if (typeof num !== 'Number' ) {
+      return 'O valor deve ser um número';
+    }
+};
 
 module.exports = calculo;
