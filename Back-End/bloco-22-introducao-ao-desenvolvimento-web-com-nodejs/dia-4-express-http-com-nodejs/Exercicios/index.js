8dc58361a13fde3f6a1b48919ac5c95b4989d1e1
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-20 10:04:00 -0300

    Adicionando Exercício 4 - Implementar rota PUT

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Exercicios/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Exercicios/index.js
@@ -23,6 +23,12 @@ app.post('/greetings', (req, res) => {
   } return res.status(401).json({ message: 'Unauthorized' });
 });
 
+// Exercício 04
+app.put('/users/:name/:age', (req, res) => {
+  const { name='Rafaela', age=32 } = req.params;
+  return res.status(200)
+    .json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
 
 app.listen(3002, () => {
   console.log('Ouvindo na porta 3002');
