9e6fb851599fa65662c3de0b56d9473c7eea2f5f
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-20 11:29:10 -0300

    Adicionando exercício 07 - Adicionando endpoint GET simpsons id

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Simpsons-Exercise/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Simpsons-Exercise/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const simpsonsUtils = require('./fs-utils');
+const e = require('express');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +16,22 @@ app.get('/simpsons', async (req, res) => {
     return res.status(500).end();
   }
 });
+
+// Exercício 07
+app.get('/simpsons/:id', async (req, res) => {
+  try {
+    const simpsons = await simpsonsUtils.getSimpsons();
+    const simpson = simpsons.find(({ id }) => id === req.params.id);
+    
+    if(!simpson) { 
+      return res.status(404).json({ message: 'simpson not found' })
+    } 
+    return res.status(200).json(simpson);
+
+  } catch (error) {
+    return res.status(500).end();
+  }
+})
 
 app.listen(3003, () => {
   console.log('Aplicação ouvindo na porta 3003');
