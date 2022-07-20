480aaa7a24f7a22428f52e4e73fe9b365eb872a1
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-20 09:58:15 -0300

    Adicionando Exercício 3 - Implementar rota POST

--- Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Exercicios/index.js
+++ Back-End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/Exercicios/index.js
@@ -9,10 +9,19 @@ app.get('/ping', (req, res) => {
   return res.status(200).json({ message: 'pong' });
 });
 
+// Exercício 02
 app.post('/hello', (req, res) => {
   const { name="Bruno" } = req.body;
   return res.status(200).json({ message: `Hello, ${name}` });
-})
+});
+
+// Exercício 03
+app.post('/greetings', (req, res) => {
+  const { name='Bruno', age=29 } = req.body;
+  if (age > 17) {
+    return res.status(200).json({ message: `Hello ${name}!` });
+  } return res.status(401).json({ message: 'Unauthorized' });
+});
 
 
 app.listen(3002, () => {
