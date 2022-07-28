94024e811cde828493ea7745259e77d27a7b3f2b
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-28 09:50:24 -0300

    Criar Modelo de Banco, Conectar e Implementar Métodos de Filtragem

---
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Book.js
@@ -0,0 +1,30 @@
+const connection = require('./connection');
+
+// Busca todos os livros do banco model_example.
+
+const getAll = async () => {
+	const [books] = await connection.execute(
+		'SELECT * FROM model_example.books;',
+	);
+	return books.map(({ id, title, author_id }) => ({
+		id,
+		title,
+		author_id: author_id,
+	}));
+};
+
+const getByAuthorId = async (authorId) => {
+	const query = 'SELECT * FROM model_example.books WHERE author_id=?;';
+	// A representação da lista om author_id significa que a query ? será substituída pelo author_id
+	const [books] = await connection.execute(query, [authorId]);
+	return books.map(({ id, title, author_id }) => ({
+		id,
+		title,
+		author_id: author_id,
+	}));
+};
+
+module.exports = {
+	getAll,
+	getByAuthorId,
+};
