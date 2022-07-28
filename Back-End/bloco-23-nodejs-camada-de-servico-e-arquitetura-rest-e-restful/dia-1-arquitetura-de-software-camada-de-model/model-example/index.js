6651b00027666b877831363b75f23416d62007ba
Bruno Disliler <bruno_disliler@outlook.com>
2022-07-28 17:42:42 -0300

    Criando e Alterando Banco de Dados Com express

--- Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ Back-End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -22,6 +22,28 @@ app.get('/authors', async (_req, res) => {
 	res.status(200).json(authors);
 });
 
+app.get('/authors/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const author = await Author.findById(id);
+
+	if (!author) return res.status(404).json({ message: 'Not found' });
+
+	res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+	const { first_name, middle_name, last_name } = req.body;
+
+	if (!Author.isValid(first_name, middle_name, last_name)) {
+		return res.status(400).json({ message: 'Dados inválidos' });
+	}
+
+	await Author.create(first_name, middle_name, last_name);
+
+	res.status(201).json({ message: 'Autor criado com sucesso! '});
+});
+
 app.get('/books', async (req, res) => {
   const { author_id } = req.query;
 
@@ -30,9 +52,17 @@ app.get('/books', async (req, res) => {
   : await Book.getAll();
 
   res.status(200).json(books);
-})
+});
 
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
 
+  const book = await Book.getById(id);
+
+  if (!book) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(book)
+});
 
 
 const PORT = process.env.PORT || 3000;
