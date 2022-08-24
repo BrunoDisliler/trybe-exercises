7bd78a63ce328db96c9de54b2935cfffebd75e8d
Bruno Disliler <bruno_disliler@outlook.com>
2022-08-24 09:45:30 -0300

    Exercício 07 e 08 - Adicionando método no controller e vinculando rota /books/:id para acesso

--- Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios-do-dia/controller/BooksController.js
+++ Back-End/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios-do-dia/controller/BooksController.js
@@ -2,11 +2,16 @@ const express = require('express');
 const BooksController = express.Router();
 const BooksService = require('../service/BookService');
 
-BooksController.get('/books', async(_req, res) => {
+BooksController.get('/books', async (_req, res) => {
   const books = await BooksService.getAll();
   res.status(200).json(books);
 });
 
-module.exports = {
-  getAll,
-};
+BooksController.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const booksId = await BooksService.getById(id);
+  if (!booksId) return res.status(404).json({ message: "Book not found" });
+  res.status(200).json(booksId);
+});
+
+module.exports = BooksController;
